af60b762a9944606b5b8e491fefa6aca81b61dda
ezequiel gallardo <ezeedge@gmail.com>
2021-04-14 01:06:55 -0300

    inicio y agregando layout

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = ({children}) => {
+    return ( 
+        <>
+            <h1>desde layout</h1>
+            {children}
+        </>
+     );
+}
+ 
+export default Layout;
---
+++ pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout'
+const Nosotros = () => {
+    return ( 
+        <>
+        <Layout>
+            <h1>Desde nosotros</h1>
+        </Layout>
+        </>
+     );
+}
+ 
+export default Nosotros;
