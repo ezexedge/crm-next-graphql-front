ba6a1eb2865a64f415e27837b4071262acd3d03d
ezequiel gallardo <ezeedge@gmail.com>
2021-04-14 01:13:48 -0300

    inicio

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -4,7 +4,7 @@ const Nosotros = () => {
     return ( 
         <>
         <Layout>
-            <h1>Desde nosotross</h1>
+            <h1>Desde nosotrossss</h1>
         </Layout>
         </>
      );
