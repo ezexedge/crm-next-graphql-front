65676fd353bcac06b2b67a2a3d3bf90825622f44
ezequiel gallardo <ezeedge@gmail.com>
2021-04-23 23:23:27 -0300

    configurando apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ApolloProvider } from '@apollo/client'
 import '../styles/globals.css'
+import client from '../config/apollo'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({Component,pageProps}) => {
+    return(
+      <ApolloProvider client={client} >
+        <Component
+        {...pageProps}
+        />
+      </ApolloProvider>
+    )
+} 
 
 export default MyApp
