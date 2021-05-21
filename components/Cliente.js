9f9036f1f6964d7be285edfb3927cd713ae336a2
ezequiel gallardo <ezeedge@gmail.com>
2021-05-21 00:35:02 -0300

    guardando cambios

--- components/Cliente.js
+++ components/Cliente.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2'
 import { gql,useMutation } from '@apollo/client'
+import Router from 'next/router'
  
 const ELIMINAR_CLIENTE = gql`
 
@@ -81,6 +82,15 @@ const Cliente = ({cliente}) => {
 
     }
 
+
+    const editarCliente = () => {
+      Router.push({
+        pathname: "/editarcliente/[id]",
+        query: {id}
+      })
+
+    }
+
     return ( 
         <>
 
@@ -92,7 +102,7 @@ const Cliente = ({cliente}) => {
                     <button
                     type="button"
                     className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-                    onClick={ () => confrimarEliminarCliente(id)}
+                    onClick={ () => confrimarEliminarCliente()}
                    >
                         Elimnar
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -101,7 +111,21 @@ const Cliente = ({cliente}) => {
 
                     </button>
                 </td>
-                
+
+                <td className="border px-4 py-2">
+                    <button
+                    type="button"
+                    className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    onClick={ () => editarCliente()}
+                   >
+                        Editar
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+</svg>
+
+                    </button>
+                </td>
+
               </tr>
         </>
      );
